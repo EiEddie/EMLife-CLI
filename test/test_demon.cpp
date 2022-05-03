2085dc8b50b74bb8ed38f95668874a6003787a60
EiEddie <jwsh2818369423@outlook.com>
2022-05-03 11:37:56 +0800

    更改测试

--- test/test_demon.cpp
+++ test/test_demon.cpp
@@ -1,5 +1,6 @@
 #include <EMLife/EMLife.h>
 #include <gtest/gtest.h>
+#include "test_maze.h"
 
 #ifdef NO_SPDLOG_LIB
 #include <cstdio>
@@ -8,46 +9,61 @@
 #endif //NO_SPDLOG_LIB
 
 TEST(EMLifeTest, test_demon) {
-	DemonManager::Demon demon(5);
-	
-	for(int i=0; i<5; i++)
-		demon.SetPath(i, {i, i});
-	
-	// 正向走一遍
-	for(int i=0; i<4; i++) {
-		Coord pos = demon.GetPos();
+	// 测试demon的移动
+	{
+		DemonManager::Demon demon(5);
+		
+		for(int i = 0; i < 5; i++)
+			demon.SetPath(i, {i, i});
+		
+		// 正向走一遍
+		for(int i = 0; i < 4; i++) {
+			Coord pos = demon.GetPos();
 #ifdef NO_SPDLOG_LIB
-		printf("[\033[34mpos\033[0m] (%d, %d)", pos.x, pos.y);
+			printf("[\033[34mpos\033[0m] (%d, %d)", pos.x, pos.y);
 #else
-		spdlog::info("[\033[34mpos\033[0m] ({:d}, {:d})", pos.x, pos.y);
+			spdlog::info("[\033[34mpos\033[0m] ({:d}, {:d})", pos.x, pos.y);
 #endif //NO_SPDLOG_LIB
-		EXPECT_EQ(Coord(i, i), pos);
-		demon.Move();
+			EXPECT_EQ(Coord(i, i), pos);
+			demon.Move();
+		}
+		// 转向
+		
+		// 反向走一遍
+		for(int i = 0; i < 4; i++) {
+			Coord pos = demon.GetPos();
+#ifdef NO_SPDLOG_LIB
+			printf("[\033[34mpos\033[0m] (%d, %d)", pos.x, pos.y);
+#else
+			spdlog::info("[\033[34mpos\033[0m] ({:d}, {:d})", pos.x, pos.y);
+#endif //NO_SPDLOG_LIB
+			EXPECT_EQ(Coord(4 - i, 4 - i), pos);
+			demon.Move();
+		}
+		// 再转向
+		
+		// 正向再走一遍
+		for(int i = 0; i < 4; i++) {
+			Coord pos = demon.GetPos();
+#ifdef NO_SPDLOG_LIB
+			printf("[\033[34mpos\033[0m] (%d, %d)", pos.x, pos.y);
+#else
+			spdlog::info("[\033[34mpos\033[0m] ({:d}, {:d})", pos.x, pos.y);
+#endif //NO_SPDLOG_LIB
+			EXPECT_EQ(Coord(i, i), pos);
+			demon.Move();
+		}
 	}
-	// 转向
 	
-	// 反向走一遍
-	for(int i=0; i<4; i++) {
-		Coord pos = demon.GetPos();
-#ifdef NO_SPDLOG_LIB
-		printf("[\033[34mpos\033[0m] (%d, %d)", pos.x, pos.y);
-#else
-		spdlog::info("[\033[34mpos\033[0m] ({:d}, {:d})", pos.x, pos.y);
-#endif //NO_SPDLOG_LIB
-		EXPECT_EQ(Coord(4-i, 4-i), pos);
-		demon.Move();
-	}
-	// 再转向
-	
-	// 正向再走一遍
-	for(int i=0; i<4; i++) {
-		Coord pos = demon.GetPos();
-#ifdef NO_SPDLOG_LIB
-		printf("[\033[34mpos\033[0m] (%d, %d)", pos.x, pos.y);
-#else
-		spdlog::info("[\033[34mpos\033[0m] ({:d}, {:d})", pos.x, pos.y);
-#endif //NO_SPDLOG_LIB
-		EXPECT_EQ(Coord(i, i), pos);
-		demon.Move();
+	// 测试添加demon
+	{
+		MazeBuilder mb;
+		DemonManager dm;
+		Maze* maze = mb.GetMaze(11, 11);
+		ShowMaze(maze);
+		
+		dm.Init(maze, {5, 5});
+		
+		mb.DestroyMaze(maze);
 	}
 }
